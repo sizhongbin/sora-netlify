83278e956a9de004a3930f4f29c2ab3ef5b1880f
sizhongbin <sizhongbin@hotmail.com>
2024-10-31 17:00:55 +0800

    Delete Index hosting

--- src/server/server.js
+++ src/server/server.js
@@ -2,9 +2,6 @@ import express from 'express'
 const server = express();
 server.use(express.json());
 
-// Index hosting
-server.use(express.static('public'));
-
 // Routers
 import hello from './routes/hello.js';
 server.use('/api/hello', hello);

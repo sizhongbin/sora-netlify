0b804d56f065cd1e7a7e1415e3a7cdeda415f19e
sizhongbin <sizhongbin@hotmail.com>
2024-10-31 16:42:56 +0800

    Test Branch deploys

--- src/server/routes/hello.js
+++ src/server/routes/hello.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router = Router();
 
-router.get("/", (req, res) => res.send("Hello World!"));
+router.get("/", (req, res) => res.send("Hello Netlify!"));
 
 export default router;
 
